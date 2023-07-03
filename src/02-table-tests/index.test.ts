0e90afb3e52af721a7b0fbc25969d92cf65de089
Pavlov Kostyantyn <realanka77@gmail.com>
2023-07-03 20:16:13 +0300

    feat: change some tests in second block

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -2,12 +2,13 @@ import { simpleCalculator, Action } from './index';
 
 const testCases = [
   { a: 1, b: 2, action: Action.Add, expected: 3 },
-  { a: 2, b: 2, action: Action.Add, expected: 4 },
-  { a: 3, b: 2, action: Action.Add, expected: 5 },
   { a: 3, b: 2, action: Action.Subtract, expected: 1 },
   { a: 3, b: 2, action: Action.Multiply, expected: 6 },
   { a: 6, b: 2, action: Action.Divide, expected: 3 },
   { a: 6, b: 2, action: Action.Exponentiate, expected: 36 },
+  { a: 6, b: 2, action: 'add', expected: null },
+  { a: '6', b: '2', action: Action.Add, expected: null },
+
   // continue cases for other actions
 ];
 
